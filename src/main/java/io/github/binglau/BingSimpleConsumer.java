a502412d96916c0b82eb514be4ea8518e4c3b3d8
BingLau7 <binglau1819@gmail.com>
2016-05-01 14:20:31 +0800

    注释

--- src/main/java/io/github/binglau/BingSimpleConsumer.java
+++ src/main/java/io/github/binglau/BingSimpleConsumer.java
@@ -2,9 +2,6 @@ package io.github.binglau;
 
 import kafka.api.*;
 import kafka.cluster.BrokerEndPoint;
-import kafka.common.TopicAndPartition;
-import kafka.javaapi.OffsetRequest;
-import kafka.javaapi.OffsetResponse;
 import kafka.javaapi.PartitionMetadata;
 import kafka.javaapi.TopicMetadata;
 import kafka.javaapi.TopicMetadataRequest;
@@ -17,9 +14,8 @@ import kafka.message.MessageAndOffset;
 
 import java.nio.ByteBuffer;
 import java.util.Collections;
-import java.util.HashMap;
 import java.util.List;
-import java.util.Map;
+import java.util.concurrent.Exchanger;
 
 /**
  * 1. 从所有活跃的broker中找出哪个是指定Topic（主题） Partition（分区）中的leader broker
@@ -32,8 +28,10 @@ public class BingSimpleConsumer {
 
     public void consumer() {
         int partition = 0;
+        // 从所有活跃的broker中找出哪个是指定Topic（主题） Partition（分区）中的leader broker
         BrokerEndPoint leaderBroker = findLeader(Config.KAFKA_ADDRESS, Config.TOPIC, partition);
 
+        // 构造消费
         SimpleConsumer simpleConsumer = new SimpleConsumer(
                 leaderBroker.host(), leaderBroker.port(), 20000, 10000, "bingSimpleConsumer");
         long startOffset = 1;
@@ -41,9 +39,11 @@ public class BingSimpleConsumer {
 
         while (true) {
             long offset = startOffset;
+            // 构造请求
             FetchRequest request = new FetchRequestBuilder()
                     .addFetch(Config.TOPIC, 0, startOffset, fetchSize).build();
 
+            // 发送请求获取数据
             FetchResponse response = simpleConsumer.fetch(request);
 
             ByteBufferMessageSet messageSet = response.messageSet(Config.TOPIC, partition);
@@ -56,11 +56,16 @@ public class BingSimpleConsumer {
                 offset = messageAndOffset.offset();
                 System.out.println("partition : " + 3 + ", offset : " + offset + "  msg : " + message);
             }
+            // 继续消费下一批
             startOffset = offset + 1;
         }
 
     }
 
+    /**
+     * 找到指定分区的leader broker
+     * @return 指定分区的leader broker
+     */
     public BrokerEndPoint findLeader(String brokerHosts, String topic, int partition) {
         BrokerEndPoint leader = findPartitionMetadata(brokerHosts, topic, partition).leader();
         System.out.println(String.format("Leader tor topic %s, partition %d is %s:%d",
@@ -68,40 +73,41 @@ public class BingSimpleConsumer {
         return leader;
     }
 
+    /**
+     * 找到指定分区的元数据
+     * @return 指定分区元数据
+     */
     private PartitionMetadata findPartitionMetadata(String brokerHosts, String topic, int partition) {
         PartitionMetadata returnMetadata = null;
         for (String brokerHost : brokerHosts.split(",")) {
             SimpleConsumer consumer = null;
-            String[] splits = brokerHost.split(":");
-            consumer = new SimpleConsumer(
-                    splits[0], Integer.valueOf(splits[1]), 100000, 64 * 1024, "leaderLookup"
-            );
-            List<String> topics = Collections.singletonList(topic);
-            TopicMetadataRequest request = new TopicMetadataRequest(topics);
-            TopicMetadataResponse response = consumer.send(request);
-            List<TopicMetadata> topicMetadatas = response.topicsMetadata();
-            for (TopicMetadata topicMetadata: topicMetadatas) {
-                for (PartitionMetadata partitionMetadata : topicMetadata.partitionsMetadata()) {
-                    returnMetadata = partitionMetadata;
+            try {
+                String[] splits = brokerHost.split(":");
+                consumer = new SimpleConsumer(
+                        splits[0], Integer.valueOf(splits[1]), 100000, 64 * 1024, "leaderLookup"
+                );
+                List<String> topics = Collections.singletonList(topic);
+                TopicMetadataRequest request = new TopicMetadataRequest(topics);
+                TopicMetadataResponse response = consumer.send(request);
+                List<TopicMetadata> topicMetadatas = response.topicsMetadata();
+                for (TopicMetadata topicMetadata : topicMetadatas) {
+                    for (PartitionMetadata partitionMetadata : topicMetadata.partitionsMetadata()) {
+                        if (partitionMetadata.partitionId() == partition) {
+                            returnMetadata = partitionMetadata;
+                            break;
+                        }
+                    }
                 }
-            }
-            if (consumer != null) {
-                consumer.close();
+            } catch (Exception e) {
+                System.out.println("Error communicating with Broker [" + brokerHost + "] to find Leader for [" + topic
+                        + ", " + partition + "] Reason: " + e);
+            } finally {
+                if (consumer != null) {
+                    consumer.close();
+                }
             }
         }
         return returnMetadata;
-    }
-
-    public long getLastOffset(SimpleConsumer consumer, String topic,
-                              int partition, String clientID, long whichTime) {
-        TopicAndPartition topicAndPartition = new TopicAndPartition(topic, partition);
-        Map<TopicAndPartition, PartitionOffsetRequestInfo> requestInfo =
-                new HashMap<TopicAndPartition, PartitionOffsetRequestInfo>();
-        requestInfo.put(topicAndPartition, new PartitionOffsetRequestInfo(whichTime, 1));
-        OffsetRequest request = new OffsetRequest(requestInfo, kafka.api.OffsetRequest.CurrentVersion(), clientID);
-        OffsetResponse response = consumer.getOffsetsBefore(request);
-        long[] offsets = response.offsets(topic, partition);
-        return offsets[0];
     }
 
     public static void main( String[] args ) {
